80120fe57aff276a165068d6eaf4bf4d5e212d92
Harvey <laozhaochaguan@gmail.com>
2020-04-15 18:52:44 +0800

    优化keydown事件

--- docs/uncompiled/term-web.js
+++ docs/uncompiled/term-web.js
@@ -2085,36 +2085,31 @@
     var $textarea = term.template.$textarea;
     events.proxy($textarea, 'keydown', function (event) {
       term.emit('keydown', event);
-      var key = event.keyCode; // Send input commands and clear input status
+      var key = event.keyCode;
 
       if (key === 13) {
         term.emit('enter', $textarea.value.trim());
         $textarea.value = '';
-      } // Exit the current command and wrap
-
+      }
 
       if (event.ctrlKey && key === 67) {
         $textarea.value = '';
         term.input();
         term.emit('abort');
-      } // Access input history
+      }
 
+      if ([37, 38, 39, 40].includes(key)) {
+        event.preventDefault();
+      }
 
       if (term.drawer.renderEditable) {
         if (key === 38) {
-          event.preventDefault();
           term.emit('history', -1);
         }
 
         if (key === 40) {
-          event.preventDefault();
           term.emit('history', 1);
         }
-      } // Disable arrow keys during input
-
-
-      if ([37, 38, 39, 40].includes(key)) {
-        event.preventDefault();
       }
     });
   }
--- src/events/keydown.js
+++ src/events/keydown.js
@@ -5,34 +5,28 @@ export default function (term, events) {
         term.emit('keydown', event);
         const key = event.keyCode;
 
-        // Send input commands and clear input status
         if (key === 13) {
             term.emit('enter', $textarea.value.trim());
             $textarea.value = '';
         }
 
-        // Exit the current command and wrap
         if (event.ctrlKey && key === 67) {
             $textarea.value = '';
             term.input();
             term.emit('abort');
         }
 
-        // Access input history
+        if ([37, 38, 39, 40].includes(key)) {
+            event.preventDefault();
+        }
+
         if (term.drawer.renderEditable) {
             if (key === 38) {
-                event.preventDefault();
                 term.emit('history', -1);
             }
             if (key === 40) {
-                event.preventDefault();
                 term.emit('history', 1);
             }
-        }
-
-        // Disable arrow keys during input
-        if ([37, 38, 39, 40].includes(key)) {
-            event.preventDefault();
         }
     });
 }
