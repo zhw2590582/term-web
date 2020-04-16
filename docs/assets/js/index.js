cedcdc906e89c7ba202ba02889f814c2c4d042d9
Harvey <laozhaochaguan@gmail.com>
2020-04-16 09:46:19 +0800

    Update index.js

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -3,6 +3,112 @@ if (isMobile) {
     window.location.href = './mobile.html';
 }
 
+function init(term) {
+    var pickrOpt = {
+        theme: 'classic',
+        swatches: [
+            'rgba(244, 67, 54, 1)',
+            'rgba(233, 30, 99, 0.95)',
+            'rgba(156, 39, 176, 0.9)',
+            'rgba(103, 58, 183, 0.85)',
+            'rgba(63, 81, 181, 0.8)',
+            'rgba(33, 150, 243, 0.75)',
+            'rgba(3, 169, 244, 0.7)',
+            'rgba(0, 188, 212, 0.7)',
+            'rgba(0, 150, 136, 0.75)',
+            'rgba(76, 175, 80, 0.8)',
+            'rgba(139, 195, 74, 0.85)',
+            'rgba(205, 220, 57, 0.9)',
+            'rgba(255, 235, 59, 0.95)',
+            'rgba(255, 193, 7, 1)',
+        ],
+        components: {
+            preview: true,
+            opacity: false,
+            hue: true,
+            interaction: {
+                hex: true,
+                rgba: true,
+                input: true,
+                save: true,
+            },
+        },
+    };
+
+    var $color = Pickr.create(
+        Object.assign(
+            {
+                el: '.color',
+                default: term.color,
+            },
+            pickrOpt,
+        ),
+    );
+
+    $color
+        .on('save', function (color) {
+            term.color = color.toHEXA().toString();
+            $color.hide();
+        })
+        .on('change', function (color) {
+            term.color = color.toHEXA().toString();
+        });
+
+    var $background = Pickr.create(
+        Object.assign(
+            {
+                el: '.background',
+                default: term.background,
+            },
+            pickrOpt,
+        ),
+    );
+
+    $background
+        .on('save', function (color) {
+            term.background = color.toHEXA().toString();
+            $background.hide();
+        })
+        .on('change', function (color) {
+            term.background = color.toHEXA().toString();
+        });
+
+    var lastIndex = -1;
+    function randomIndex() {
+        var index = parseInt(Math.random() * 19 + 1, 10);
+        if (index === lastIndex) return randomIndex();
+        return index;
+    }
+
+    var $watermark = document.querySelector('.watermark');
+    $watermark.addEventListener('click', function () {
+        term.watermark = `/assets/img/watermark${randomIndex()}.png`;
+    });
+
+    var $pip = document.querySelector('.pip');
+    var $video = document.querySelector('.video');
+    $pip.addEventListener('click', function () {
+        var canvas = term.template.$canvas;
+        var steam = canvas.captureStream(30);
+        $video.srcObject = steam;
+        setTimeout(function () {
+            $video.play();
+            if (document.pictureInPictureEnabled) {
+                $video.requestPictureInPicture();
+            } else if ($video.webkitSupportsPresentationMode) {
+                $video.webkitSetPresentationMode('picture-in-picture');
+            } else {
+                $video.srcObject = null;
+            }
+        }, 1000);
+    });
+
+    var $debug = document.querySelector('.debug');
+    $debug.addEventListener('click', function () {
+        term.debug = !term.debug;
+    });
+}
+
 var mirror = CodeMirror(document.querySelector('.code'), {
     lineNumbers: true,
     mode: 'javascript',
@@ -25,109 +131,5 @@ fetch(url)
     .then(function (text) {
         mirror.setValue(text);
         eval(text);
+        init(Term.instances[0]);
     });
-
-var pickrOpt = {
-    theme: 'classic',
-    swatches: [
-        'rgba(244, 67, 54, 1)',
-        'rgba(233, 30, 99, 0.95)',
-        'rgba(156, 39, 176, 0.9)',
-        'rgba(103, 58, 183, 0.85)',
-        'rgba(63, 81, 181, 0.8)',
-        'rgba(33, 150, 243, 0.75)',
-        'rgba(3, 169, 244, 0.7)',
-        'rgba(0, 188, 212, 0.7)',
-        'rgba(0, 150, 136, 0.75)',
-        'rgba(76, 175, 80, 0.8)',
-        'rgba(139, 195, 74, 0.85)',
-        'rgba(205, 220, 57, 0.9)',
-        'rgba(255, 235, 59, 0.95)',
-        'rgba(255, 193, 7, 1)',
-    ],
-    components: {
-        preview: true,
-        opacity: false,
-        hue: true,
-        interaction: {
-            hex: true,
-            rgba: true,
-            input: true,
-            save: true,
-        },
-    },
-};
-
-var $color = Pickr.create(
-    Object.assign(
-        {
-            el: '.color',
-            default: '#b0b2b6',
-        },
-        pickrOpt,
-    ),
-);
-
-$color
-    .on('save', function (color) {
-        Term.instances[0].color = color.toHEXA().toString();
-        $color.hide();
-    })
-    .on('change', function (color) {
-        Term.instances[0].color = color.toHEXA().toString();
-    });
-
-var $background = Pickr.create(
-    Object.assign(
-        {
-            el: '.background',
-            default: 'rgb(42, 39, 52)',
-        },
-        pickrOpt,
-    ),
-);
-
-$background
-    .on('save', function (color) {
-        Term.instances[0].background = color.toHEXA().toString();
-        $background.hide();
-    })
-    .on('change', function (color) {
-        Term.instances[0].background = color.toHEXA().toString();
-    });
-
-var lastIndex = -1;
-function randomIndex() {
-    var index = parseInt(Math.random() * 19 + 1, 10);
-    if (index === lastIndex) return randomIndex();
-    return index;
-}
-
-var $watermark = document.querySelector('.watermark');
-$watermark.addEventListener('click', function () {
-    Term.instances[0].watermark = `/assets/img/watermark${randomIndex()}.png`;
-});
-
-var $pip = document.querySelector('.pip');
-var $video = document.querySelector('.video');
-$pip.addEventListener('click', function () {
-    var canvas = Term.instances[0].template.$canvas;
-    var steam = canvas.captureStream(30);
-    $video.srcObject = steam;
-    setTimeout(function () {
-        $video.play();
-        if (document.pictureInPictureEnabled) {
-            $video.requestPictureInPicture();
-        } else if ($video.webkitSupportsPresentationMode) {
-            $video.webkitSetPresentationMode('picture-in-picture');
-        } else {
-            $video.srcObject = null;
-        }
-    }, 1000);
-});
-
-var $debug = document.querySelector('.debug');
-$debug.addEventListener('click', function () {
-    var term = Term.instances[0];
-    term.debug = !term.debug;
-});
